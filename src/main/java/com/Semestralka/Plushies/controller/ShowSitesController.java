73f69281abc814b0626a5626b6b833440a6e773c
sasenkauwu <mokosovje@gmail.com>
2024-01-16 19:06:00 +0100

    vloženie views

--- src/main/java/com/Semestralka/Plushies/controller/ShowSitesController.java
+++ src/main/java/com/Semestralka/Plushies/controller/ShowSitesController.java
@@ -19,8 +19,49 @@ public class ShowSitesController {
         return "login";
     }
 
-    @GetMapping("/getUpdateDelete")
-    public String showGetUpdateDelete() {
-        return "getUpdateDelete";
+    @GetMapping("/adminInterface")
+    public String showaAdminInterface() {
+        return "adminInterface";
     }
+
+    @GetMapping("/profile")
+    public String showProfile() {
+        return "profile";
+    }
+
+    @GetMapping("/home")
+    public String showHome() {
+        return "home";
+    }
+
+    @GetMapping("/aboutUs")
+    public String showAboutUs() {
+        return "aboutUs";
+    }
+
+    @GetMapping("/products")
+    public String showproducts() {
+        return "products";
+    }
+
+    @GetMapping("/contacts")
+    public String showContacts() {
+        return "contacts";
+    }
+
+    @GetMapping("/returnPolicy")
+    public String showReturnPolicy() {
+        return "returnPolicy";
+    }
+
+    @GetMapping("/privacyPolicy")
+    public String showPrivacyPolicy() {
+        return "privacyPolicy";
+    }
+
+    @GetMapping("/cart")
+    public String showCart() {
+        return "cart";
+    }
+
 }
